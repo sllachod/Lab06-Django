f85498471b23e9153a10caa0f299e055355f124b
nuevo_63 <ajrahuacso@gmail.com>
2024-05-26 00:20:14 -0500

    Se crearon las tablas a partir del modelo

--- registroCurso/models.py
+++ registroCurso/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
+
+class Nota(models.Model):
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nota = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"Nota de {self.alumno.nombre} en {self.curso.nombre}: {self.nota}"
