4a5d9e905579935e949bc2cff792f8935ae67347
nuevo_63 <ajrahuacso@gmail.com>
2024-05-26 13:47:21 -0500

    Se crean las url y templates para la nota

--- registroCurso/urls.py
+++ registroCurso/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('lista/', views.lista_alumnos, name='lista_alumnos'),  # URL para listar todos los alumnos
     path('crearCur/', views.crear_curso, name='crear_curso'),  # URL para crear un nuevo curso
     path('listaCur/', views.lista_cursos, name='lista_cursos'),  # URL para listar todos los cursos
+    path('crearNota/', views.crear_nota, name='crear_nota'),  # URL para crear un nueva nota
+    path('listaNota/', views.lista_notas_alumno, name='lista_notas_alumno'),  # URL para listar todos las notas
 ]
