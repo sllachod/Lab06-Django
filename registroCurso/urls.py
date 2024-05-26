3a4ef7d84ebdb6926e4f0d0614cb8c7c779c657a
nuevo_63 <ajrahuacso@gmail.com>
2024-05-26 16:07:57 -0500

    Se agregaron pruebas

--- registroCurso/urls.py
+++ registroCurso/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('crear/', views.crear_alumno, name='crear_alumno'),  # URL para crear un nuevo alumno
-    path('lista/', views.lista_alumnos, name='lista_alumnos'),  # URL para listar todos los alumnos
-    path('crearCur/', views.crear_curso, name='crear_curso'),  # URL para crear un nuevo curso
-    path('listaCur/', views.lista_cursos, name='lista_cursos'),  # URL para listar todos los cursos
-    path('crearNota/', views.crear_nota, name='crear_nota'),  # URL para crear un nueva nota
-    path('listaNota/', views.lista_notas_alumno, name='lista_notas_alumno'),  # URL para listar todos las notas
+    path('crear/', views.crear_alumno, name='crear_alumno'),  
+    path('lista/', views.lista_alumnos, name='lista_alumnos'),  
+    path('crearCur/', views.crear_curso, name='crear_curso'),  
+    path('listaCur/', views.lista_cursos, name='lista_cursos'),  
+    path('crearNota/', views.crear_nota, name='crear_nota'),  
+    path('listaNota/', views.lista_notas_alumno, name='lista_notas_alumno'),  
 ]
